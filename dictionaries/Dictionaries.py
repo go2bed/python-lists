a8ebd7d128afad251e0ea48bcecb58520791cf2a
Andrey_Chadov <androusj@mail.ru>
2017-09-25 22:17:36 +0600

    [IMP] Implemented some dictionary actions

---
+++ dictionaries/Dictionaries.py
@@ -0,0 +1,9 @@
+my_dict = {'key': 'value', 'key2': 'value2'}
+print(my_dict['key'])
+my_dict = {'k1': '123', 'k2': 3.14, 'k3': 'string'}
+print(my_dict['k3'])
+print(my_dict['k3'][::-1])
+print(my_dict['k3'].upper())
+print(my_dict['k1'])
+my_dict['k1'] = my_dict['k1'] - 120
+print(my_dict['k1'])
