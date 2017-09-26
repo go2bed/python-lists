5f6dc39d5d4ef6219cd100bbe52f4f5a4317e539
Andrey_Chadov <androusj@mail.ru>
2017-09-26 08:12:44 +0600

    [IMP] some dictonaties

--- dictionaries/Dictionaries.py
+++ dictionaries/Dictionaries.py
@@ -1,19 +1,33 @@
 my_dict = {'key': 'value', 'key2': 'value2'}
 print(my_dict['key'])
+
 my_dict = {'k1': 123, 'k2': 3.14, 'k3': 'string'}
 print(my_dict['k3'])
 print(my_dict['k3'][::-1])
 print(my_dict['k3'].upper())
+
 print(my_dict['k1'])
+
 my_dict['k1'] = my_dict['k1'] - 120
 print(my_dict['k1'])
 my_dict['k1'] += 100
 print(my_dict['k1'])
+
 d = {}
 d['animal'] = 'Dog'
 d['answer'] = 42
+
 print(d)
 d = {'k1': {'nestkey': {'subnestkey': 'value'}}}
 print(d['k1'])
 print(d['k1']['nestkey'])
 print(d['k1']['nestkey']['subnestkey'].upper())
+
+d['k1'] = 1
+d['k2'] = 2
+d['k3'] = 3
+print(d)
+
+print(d.keys())
+print(d.values())
+print(d.items())
