4b2f3f20531ca86e23a768b099d820478080553f
Andrey_Chadov <androusj@mail.ru>
2017-09-30 08:28:10 +0600

    [IMP] tuples

---
+++ tuples/Tuples.py
@@ -0,0 +1,31 @@
+list = [1, 2, 3]
+tuple = (1, 2, 3)
+print(len(tuple))
+
+tuple = ('one', 2)
+print(tuple[0])
+
+print(tuple[-1])
+
+print(tuple.index('one'))
+
+print(tuple.index(2))
+
+print(tuple.count('one'))
+
+tuple = (1, 1, 2, 2)
+
+print(tuple.count(1))
+
+l = [1, 2, 3]
+
+t = (1, 2, 3)
+
+l[0] = 's'
+
+print(l)
+
+#t[0] = 'r'  ##Error, cause tuple is immitable
+
+
+
